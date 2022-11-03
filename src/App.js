923752e51436ddeca67427745da1ed602082a003
Vlad <andreivladpaul@gmail.com>
2022-11-04 00:02:45 +0100

    react app setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  const [search, setSearch] = useState('everything');
+  const [loading, setLoading] = useState(true);
+
+  const getArticles = () => {
+    axios.get(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${search}&api-key=XGIAAFNN03ZdUVs6evngSmccuoJvdPA2`)
+      .then(res => {
+        const articles = res.data.response.docs;
+        setArticles(articles)
+      })
+  }
+
+  useEffect(() => {
+    const fetchArticles = () => {
+      try {
+        getArticles();
+      }
+      catch (error) {
+        console.error(error)
+      }
+    }
+    fetchArticles();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+    </>
   );
 }
 
